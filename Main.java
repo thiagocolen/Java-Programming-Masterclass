e674af6caf4df438515ac901020d2f9e1db737a0
Thiago Colen <thiago.souzacolen@gmail.com>
2022-06-16 18:31:10 +0000

    more primitive types - floar, double, char, boolean and String*

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 
-class Main {  
-  public static void main(String args[]) { 
+class Main {
+  public static void main(String args[]) {
     primitiveTypes myPrimitiveTypesLesson = new primitiveTypes();
-  } 
-} 
+  }
+}
