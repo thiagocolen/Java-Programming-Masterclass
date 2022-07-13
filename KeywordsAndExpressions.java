22b459e944585ec83fc57974e05af3f817ee0cd7
Thiago Colen <thiago.souzacolen@gmail.com>
2022-07-13 13:00:06 -0300

    Keywords and Expressions

---
+++ KeywordsAndExpressions.java
@@ -0,0 +1,35 @@
+public class KeywordsAndExpressions {
+
+    public void main() {
+        System.out.println("\n \n");
+        System.out.println("Java Programming Masterclass #3 - Keywords and Expressions");
+        System.out.println("------------------------------------------------- \n");
+
+        System.out.println("List of Java Keywords");
+        System.out.println("https://en.wikipedia.org/wiki/List_of_Java_keywords");
+
+        System.out.println("\n \n");
+        System.out.println("Expressions examples");
+        System.out.println("------------------------------------------------- \n");
+
+        int score = 100;
+        if(score > 99) {
+            System.out.println("Your score is greater than 99!");
+            score = 0;
+        }
+
+        System.out.println("Now your score is 0.\n");
+
+        System.out.println("------------------------------------------------- \n");
+        System.out.println("On code above the following parts are expressions:");
+        System.out.println("score = 100");
+        System.out.println("score > 99");
+        System.out.println("'Your score is greater than 99!'");
+        System.out.println("score = 0");
+        System.out.println("'Now your score is 0.'");
+
+    }
+
+
+
+}
--- Main.java
+++ Main.java
@@ -2,9 +2,12 @@ class Main {
   public static void main(String args[]) {
     PrimitiveTypes myPrimitiveTypesLesson = new PrimitiveTypes();
     myPrimitiveTypesLesson.main();
+
     Operators myOperatorsLesson = new Operators();
     myOperatorsLesson.main();
 
+    KeywordsAndExpressions myKeywordsAndExpressionsLesson = new KeywordsAndExpressions();
+    myKeywordsAndExpressionsLesson.main();
 
   }
 }
